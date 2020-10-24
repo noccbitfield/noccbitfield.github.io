79fa58c463633cf7f6dcd5ea15a96312b9810471
noccbitfield <nocchabat@gmail.com>
2020-10-24 13:25:02 -0700

    device check

--- js/fieldhome.js
+++ js/fieldhome.js
@@ -1,3 +1,11 @@
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+  // true for mobile device
+  document.write("mobile device");
+}else{
+  // false for not mobile device
+  document.write("not mobile device");
+}
+
 var tID; 
 
 function stopAnimate() {
--- js/userscript.js
+++ js/userscript.js
@@ -1,4 +1,4 @@
-/*set max to datepicker*/
+/* 	FIX DATE PICKER */
 document.getElementById("bday").max = new Date(new Date().getTime() - new Date().getTimezoneOffset() * 60000).toISOString().split("T")[0];
 
 
