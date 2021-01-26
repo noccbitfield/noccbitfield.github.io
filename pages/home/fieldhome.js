3494918bbc116c5ce29dcbacfe94a0443e558cf6
noccbitfield <nocchabat@gmail.com>
2021-01-25 16:53:53 -0800

    mobile fix

--- pages/home/fieldhome.js
+++ pages/home/fieldhome.js
@@ -2,7 +2,7 @@
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
     //document.write("mobile device");
-    window.location.href = "../mobile/mobile.html";
+    window.location.href = "../mobile/index.html";
   }else{
     // false for not mobile device
     //document.write("not mobile device");
